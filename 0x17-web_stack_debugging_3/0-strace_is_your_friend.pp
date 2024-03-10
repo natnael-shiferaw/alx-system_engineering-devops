27671a2452bc1b8d40d8212c7d8058f1cb71b92c
Natnael Shiferaw <130279239+natnael-shiferaw@users.noreply.github.com>
2024-03-11 00:02:54 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # Fixes Apache 500 error by correcting a typo in WordPress configuration file
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
-}
-
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
